86cd5fd9c4201a2219bc2580da8fdb1c5fdea4b3
Carlos Martinez <martinez96carlos@gmail.com>
2020-05-18 16:00:23 -0400

    restore files

--- src/controllers/recolection.controller.js
+++
@@ -1,9 +0,0 @@
-//conectarse a la base de datos importar pool del modulo de postgresql
-const {Pool} = require('pg');
-const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'admin',
-    database: 'databaseTest3',
-    port: '5432'
-})
