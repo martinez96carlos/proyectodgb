87cb3728812a48a250f631e83169f4046e813063
Carlos Martinez <martinez96carlos@gmail.com>
2020-06-02 18:51:34 -0400

    nombre apellido y ciudad concatenado top 5

--- src/routes/datamart.js
+++ src/routes/datamart.js
@@ -11,16 +11,16 @@ const  {getVolumenPorResiduoGeneral,
     getVolumenPorMesPersonal } = require('../controllers/dataMartController');
 
 //graficos generales
-router.get('/gone',getVolumenPorResiduoGeneral );
-router.get('/gtwo', getVolumenPorTipoVivienda);
-router.get('/gthree',getVolumenPorMes );
-router.get('/gfour', getVolumenPorCiudad);
-router.get('/gfive', getTopFiveRecolectors);
+router.get('/gone',getVolumenPorResiduoGeneral ); //grafico 1 en el cel
+router.get('/gtwo', getVolumenPorTipoVivienda); // grafico 5 en el cel
+router.get('/gthree',getVolumenPorMes ); //grafico 4 en el cel
+router.get('/gfour', getVolumenPorCiudad); //grafico 2 en el cel
+router.get('/gfive', getTopFiveRecolectors); // grafico 3 en el cel  //concatenar nombre apellido y eliminar ciudad
 
 //graficos personales
-router.get('/pone/:id', getVolumenPorResiduoPersonal);
-router.get('/ptwo/:id', getVolumenPorViviendaPersonal);
-router.get('/pthree/:id', getVolumenPorMesPersonal);
+router.get('/pone/:id', getVolumenPorResiduoPersonal); //grafico 1 en el cel
+router.get('/ptwo/:id', getVolumenPorViviendaPersonal); // grafico 3 en el cel
+router.get('/pthree/:id', getVolumenPorMesPersonal); // grafico 2 en el cel
 
 
 module.exports = router;
